124d1adef85e6129852b7b63b96ec76eb8672d19
Alexander Vaagan <alexander.vaagan@inmeta.no>
2018-10-18 16:13:24 +0200

    Updates

--- dataset.py
+++ dataset.py
@@ -5,6 +5,18 @@ import numpy as np
 from PIL import Image
 from pycocotools.coco import COCO
 from torchvision import transforms
+from torchvision.datasets import ImageFolder
+
+
+def get_folder_loader(dataroot, transform, batch_size, shuffle, num_workers):
+    dataset = ImageFolder(root=dataroot,
+                               transform=transform, target_transform=None)
+    # Create the dataloader
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
+                                             shuffle=shuffle, num_workers=num_workers)
+
+    return dataloader
+
 
 
 class CocoDataset(data.Dataset):
@@ -45,37 +57,7 @@ class CocoDataset(data.Dataset):
         return len(self.ids)
 
 
-def collate_fn(data):
-    """Creates mini-batch tensors from the list of tuples (image, caption).
-
-    We should build custom collate_fn rather than using default collate_fn,
-    because merging caption (including padding) is not supported in default.
-    Args:
-        data: list of tuple (image, caption).
-            - image: torch tensor of shape (3, 256, 256).
-            - caption: torch tensor of shape (?); variable length.
-    Returns:
-        images: torch tensor of shape (batch_size, 3, 256, 256).
-        targets: torch tensor of shape (batch_size, padded_length).
-        lengths: list; valid length for each padded caption.
-    """
-    # Sort a data list by caption length (descending order).
-    data.sort(key=lambda x: len(x[1]), reverse=True)
-    images, captions = zip(*data)
-
-    # Merge images (from tuple of 3D tensor to 4D tensor).
-    images = torch.stack(images, 0)
-
-    # Merge captions (from tuple of 1D tensor to 2D tensor).
-    lengths = [len(cap) for cap in captions]
-    targets = torch.zeros(len(captions), max(lengths)).long()
-    for i, cap in enumerate(captions):
-        end = lengths[i]
-        targets[i, :end] = cap[:end]
-    return images, targets, lengths
-
-
-def get_loader(root, json, transform, batch_size, shuffle, num_workers, categories):
+def get_coco_loader(root, json, transform, batch_size, shuffle, num_workers, categories):
     """Returns torch.utils.data.DataLoader for custom coco dataset."""
     # COCO caption dataset
     coco = CocoDataset(root=root,
@@ -94,6 +76,16 @@ def get_loader(root, json, transform, batch_size, shuffle, num_workers, categori
                                               num_workers=num_workers)
     return data_loader
 
+def toGray(img):
+    result = np.zeros((*img.size, 3))
+    gray = img.convert("L")
+    result[:,:, 0] = gray
+    result[:,:, 1] = gray
+    result[:,:, 2] = gray
+
+    result = np.concatenate([img, result], 1)
+    return result
+
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
@@ -102,15 +94,6 @@ if __name__ == '__main__':
     image_dir = 'z:/Dataset/coco/val/orig/val2017/'
     ann_file = 'z:/Dataset/coco/annotations/instances_val2017.json'
 
-    def toGray(img):
-        result = np.zeros((*img.size, 3))
-        gray = img.convert("L")
-        result[:,:, 0] = gray
-        result[:,:, 1] = gray
-        result[:,:, 2] = gray
-
-        result = np.concatenate([img, result], 1)
-        return result
 
     tf = transforms.Compose([
         transforms.Resize(256),
@@ -119,7 +102,7 @@ if __name__ == '__main__':
         transforms.ToTensor(),
     ])
 
-    dataloader = get_loader(root = image_dir, json=ann_file, batch_size=4, shuffle=True, num_workers=1, transform=tf, categories=['truck'])
+    dataloader = get_coco_loader(root = image_dir, json=ann_file, batch_size=4, shuffle=True, num_workers=1, transform=tf, categories=['truck'])
 
     for i_batch, sample_batched in enumerate(dataloader):
         print(i_batch, sample_batched.size())
--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import torch.nn as nn
 from torch.nn import init
 import functools
 from torch.optim import lr_scheduler
+from torchvision.transforms import transforms
 
-from dataset import get_loader
+from dataset import get_coco_loader, get_folder_loader, toGray
+from image_pool import ImagePool
 from network import UnetGenerator, NLayerDiscriminator, GANLoss
 
-image_dir = '/home/alex/Datasets/coco/val/val2017'
-ann_file = '/home/alex/Datasets/coco/annotations/instances_val2017.json'
+image_dir = '/home/alex/Datasets/coco/val/train'
+# ann_file = '/home/alex/Datasets/coco/annotations/instances_val2017.json'
 
 lr = 0.0002
 beta1 = 0.5
@@ -16,13 +18,17 @@ beta1 = 0.5
 ngpu = 1
 no_lsgan = False
 epocs = 5
+pool_size = 50
+image_size = 256
 
 
 # Decide which device we want to run on
-device = torch.device("cuda:1" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
+device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
 
 netG = UnetGenerator(input_nc=3, output_nc=3, num_downs=7, ngf=64, norm_layer=nn.BatchNorm2d, use_dropout=False)
 netD = NLayerDiscriminator(input_nc=3, ndf=64, n_layers=3, norm_layer=nn.BatchNorm2d, use_sigmoid=False)
+
+fake_AB_pool = ImagePool(pool_size)
 
 criterionGAN = GANLoss(use_lsgan=not no_lsgan).to(device)
 criterionL1 = torch.nn.L1Loss()
@@ -36,14 +42,21 @@ optimizer_D = torch.optim.Adam(netD.parameters(),
 optimizers.append(optimizer_G)
 optimizers.append(optimizer_D)
 
+trans = transforms.Compose([
+        transforms.Resize(image_size),
+        transforms.CenterCrop(image_size),
+        transforms.Lambda(toGray),
+        # transforms.ToTensor(),
+    ])
+dataset = get_folder_loader(dataroot=image_dir, batch_size=4, shuffle=True, num_workers=1, transform=trans)
 
-dataset = get_loader(root=image_dir, json=ann_file, batch_size=4, shuffle=True, num_workers=1, transform=tf, categories=['truck'])
-
-for epoc in epocs:
+for epoc in range(epocs):
 
     for i, data in enumerate(dataset):
         # model.set_input(data)
         # model.optimize_parameters()
+        data, _ = data
+
         real_A = input['A' if AtoB else 'B'].to(self.device)
         real_B = input['B' if AtoB else 'A'].to(self.device)
 
